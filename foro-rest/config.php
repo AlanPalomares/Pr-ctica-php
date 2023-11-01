56c3c56a1974ea98bd117187ca419e2090383c1d
AlanPalomares <143009510+AlanPalomares@users.noreply.github.com>
2023-11-01 00:02:19 -0600

    Add files via upload

---
+++ foro-rest/config.php
@@ -0,0 +1,9 @@
+<?php
+class Config{
+    const SERVER = "localhost";
+    const DBNAME = "foro";
+    const USER = "root";
+    const PASS = "";
+
+    const SECRET = "12345678901234561234567890123456";
+}
